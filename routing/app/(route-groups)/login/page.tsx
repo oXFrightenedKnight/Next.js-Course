6aaa3a793e79c01ce761bb5cc2a8f5dc51f8625f
Vlad Stepanov <prolda31@gmail.com>
2025-08-05 15:51:21 -0700

    save

---
+++ routing/app/(route-groups)/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return <h1>Log In</h1>;
+}
+
+// Basically (route-groups) will exclude the route-groups folder from the url path.
+// Wrap any folder with parenthis and it will be excluded from the URL path.
+
+// IF YOU HAVE PROBLEMS WITH RENAMING THE FOLDER, STOP THE SERVER, RENAME THE FOLDER, AND
+// RESTART THE SERVER
---
+++ routing/app/(route-groups)/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function SignUp() {
+  return <h1>Sign Up</h1>;
+}
+
+// Basically (route-groups) will exclude the route-groups folder from the url path.
+// Wrap any folder with parenthis and it will be excluded from the URL path.
+
+// IF YOU HAVE PROBLEMS WITH RENAMING THE FOLDER, STOP THE SERVER, RENAME THE FOLDER, AND
+// RESTART THE SERVER

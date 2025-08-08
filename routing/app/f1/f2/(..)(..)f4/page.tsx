573954a96b11ddd33504791c7a7188dd4a1d1401
Vlad Stepanov <prolda31@gmail.com>
2025-08-07 17:03:42 -0700

    sv

---
+++ routing/app/f1/f2/(..)(..)f4/page.tsx
@@ -0,0 +1,6 @@
+export default function InterceptedF4() {
+  return <h1>(..)(..) Intercepted F4</h1>;
+}
+
+// every (..) in the name tells next.js how many levels above is your desired component
+// (.) tells next.js that the intercepted component is on the same level

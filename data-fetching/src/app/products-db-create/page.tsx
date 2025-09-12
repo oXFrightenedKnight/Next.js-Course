518da9f78de1d95cd37c5590830289b25f7797b0
Frightened Knight <prolda31@gmail.com>
2025-09-12 14:32:05 -0700

    sv

---
+++ data-fetching/src/app/products-db-create/page.tsx
@@ -0,0 +1,36 @@
+import { addProduct } from "@/prisma-db";
+import { redirect } from "next/navigation";
+import { Submit } from "@/components/submit";
+// This code never reaches the browser. Its like a built in API endpoint in your
+// component.
+
+export default function AddProductPage() {
+  async function createProduct(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+    const price = formData.get("price") as string;
+    const description = formData.get("description") as string;
+
+    await addProduct(title, parseInt(price), description);
+    redirect("/products-db");
+  }
+
+  return (
+    <form action={createProduct} className="p-4 space-y-4 max-w-96">
+      <label className="text-white">
+        Title
+        <input type="text" className="block w-full p-2 text-black border rounded" name="title" />
+      </label>
+      <label className="text-white">
+        Price
+        <input type="number" className="block w-full p-2 text-black border rounded" name="price" />
+      </label>
+      <label className="text-white">
+        Description
+        <textarea className="block w-full p-2 text-black border rounded" name="description" />
+      </label>
+      <Submit></Submit>
+    </form>
+  );
+}

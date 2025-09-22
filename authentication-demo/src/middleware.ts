20252a9d613f97fba441c24578917660bbcc21cc
Frightened Knight <prolda31@gmail.com>
2025-09-22 15:20:21 -0700

    sv

--- authentication-demo/src/middleware.ts
+++ authentication-demo/src/middleware.ts
@@ -1,10 +1,19 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // const isProtectedRoute = createRouteMatcher(["/user-profile"]);
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
 export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
+
+  if (isAdminRoute(req) && (await auth()).sessionClaims?.metadata.role !== "admin") {
+    const url = new URL("/", req.url);
+    return NextResponse.redirect(url);
+  }
+
   if (!userId && !isPublicRoute(req)) {
     // Add custom logic before redirecting
 
